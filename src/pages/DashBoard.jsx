4641383b4865ee6c3add3a3944b2a34e2569c00f
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-22 18:55:04 +0530

    Modify User DashBoard

--- src/pages/DashBoard.jsx
+++ src/pages/DashBoard.jsx
@@ -1,8 +1,26 @@
+import { message } from "antd";
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
 const DashBoard = () =>{
+    const mynav = useNavigate();
+    const [username,setusername] = useState("");
+    useEffect(()=>{
+        const name = localStorage.getItem("name")
+        setusername(name);
+    },[])
+
+    const logout = () =>{
+        localStorage.clear();
+        message.warning("logout successfull!!!")
+        mynav("/")
+    }
     return(
         <>
             <div id="dashboard">
-                <h1><strong>Welcome Back to DashBoard</strong></h1>
+                <h1>Welcome Mr. " {username} "</h1>
+                <Button onClick={logout} variant="outline-primary">LogOut</Button>
             </div>
         </>
     )
