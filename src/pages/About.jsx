4993bb209bfd76e4b33151f678cfadb5fe711e61
SURAJ_KUMAR <surajk38656@gmail.com>
2024-09-22 18:54:42 +0530

    Modify AboutPage

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,24 @@
 const About = () =>{
     return(
         <>
+        <br />
         <center><h1>About Page</h1></center>
+        <br />
+        <div id="about">
+        <div id="box1">
+        <h3>About Product</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, a. Dicta accusamus ut eligendi doloribus. Eveniet voluptas maxime natus veritatis ad. Fugit iste dolor harum? Vitae temporibus veniam, nulla, repudiandae ratione aspernatur voluptas est voluptatibus doloribus et sint ipsam amet commodi, quasi doloremque voluptate <br/> consectetur? Hic in odit sint beatae omnis non iste quis ad recusandae, nisi, ipsum illum temporibus <br/> tempore asperiores vel? Pariatur similique nihil molestias esse aliquid, corrupti sed commodi libero voluptatum alias id, mollitia facere earum. Explicabo, impedit nesciunt odio qui optio soluta omnis, molestiae, repudiandae eos accusantium beatae. Dicta, fugiat <br/> animi quam quaerat eius vel laboriosam aliquam, suscipit corporis tenetur voluptas nisi? Nulla, sed ullam? Quia eaque sequi delectus officiis voluptate vero esse sunt debitis quis ratione eius animi, illo <br/> error consequuntur praesentium placeat voluptates unde quidem ipsum reprehenderit adipisci facilis? Veniam veritatis saepe doloremque maiores odio libero eius earum ducimus esse in iure deleniti, minus dicta explicabo nostrum facere illo harum odit quos eligendi <br/> hic voluptatum. Facere nobis eius asperiores provident nulla, deleniti sequi at beatae molestias atque temporibus reprehenderit, nam delectus, sapiente itaque quo a nostrum natus ratione! Doloremque  consequatur beatae facilis error fuga officia nihil quam, perspiciatis quas repudiandae ut laborum similique et!</p>
+        </div>
+        <div id="box1">
+        <h3>About Prices</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, a. Dicta accusamus ut eligendi doloribus. Eveniet voluptas maxime natus veritatis ad. Fugit iste dolor harum? Vitae temporibus veniam, nulla, repudiandae <br/> ratione aspernatur voluptas est voluptatibus doloribus et sint ipsam amet commodi, quasi doloremque voluptate consectetur? Hic in odit sint beatae omnis non iste quis ad recusandae, nisi, ipsum illum temporibus tempore asperiores vel? Pariatur similique nihil molestias esse aliquid, corrupti sed commodi libero voluptatum alias id, mollitia facere earum. Explicabo, impedit nesciunt odio qui optio soluta omnis, molestiae, repudiandae eos accusantium beatae.<br/> Dicta, fugiat animi quam quaerat eius vel laboriosam aliquam, suscipit corporis tenetur voluptas nisi? Nulla, sed ullam? Quia eaque sequi delectus officiis voluptate vero esse sunt debitis quisratione eius animi, illo error consequuntur praesentium placeat voluptates unde quidem ipsum reprehenderit adipisci facilis? Veniam veritatis saepe doloremque maiores odio libero eius earum ducimus esse in <br/> iure deleniti, minus dicta explicabo nostrum facere illo harum odit quos eligendi hic voluptatum. Facere nobis eius asperiores provident nulla, deleniti sequi at beatae molestias atque temporibus reprehenderit, nam delectus, sapiente itaque quo a nostrum natus ratione! Doloremque consequatur beatae facilis error fuga officia nihil quam, perspiciatis quas repudiandae ut laborum similique et!</p>
+        </div>
+        <div id="box1">
+        <h3>About Offers</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui, a. Dicta accusamus ut eligendi doloribus. Eveniet voluptas maxime natus veritatis ad. Fugit iste dolor harum? Vitae temporibus veniam, nulla, repudiandae ratione aspernatur voluptas est voluptatibus doloribus et sint ipsam amet commodi, quasi doloremque <br/> voluptate consectetur? Hic in odit sint beatae omnis non iste quis ad recusandae, nisi, ipsum illum temporibus tempore asperiores vel? Pariatur similique nihil molestias esse aliquid, corrupti sed commodi <br/> libero voluptatum alias id, mollitia facere earum. Explicabo, impedit nesciunt odio qui optio soluta omnis, molestiae, repudiandae eos accusantium beatae. Dicta, fugiat animi quam quaerat eius vel laboriosam aliquam, suscipit corporis tenetur voluptas nisi? Nulla,<br/> sed ullam? Quia eaque sequi delectus officiis voluptate vero esse sunt debitis quis ratione eius animi, illo error consequuntur praesentium placeat voluptates unde quidem ipsum reprehenderit adipisci facilis? Veniam veritatis saepe doloremque maiores odio libero eius earum ducimus esse in iure deleniti,<br/> minus dicta explicabo nostrum facere illo harum odit quos eligendi hic voluptatum. Facere nobis eius asperiores provident nulla, deleniti sequi at beatae molestias atque temporibus reprehenderit, nam delectus,<br/> sapiente itaque quo a nostrum natus ratione! Doloremque consequatur beatae facilis error fuga officia nihil quam, perspiciatis quas repudiandae ut laborum similique et!</p>
+        </div>
+        </div>
+        <br />
         </>
     )
 }
